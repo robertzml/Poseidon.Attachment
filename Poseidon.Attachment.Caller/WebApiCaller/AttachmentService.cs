7f88391cb5acf3804b3601d7f1acf617f55e862d
robertzml <robertzml@126.com>
2017-07-04 17:31:33 +0800

    Add Attachment web api service.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Attachment.Caller/WebApiCaller/AttachmentService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poseidon.Attachment.Caller.WebApiCaller
+{
+    using Poseidon.Base.Framework;
+    using Poseidon.Attachment.Caller.Facade;
+    using Poseidon.Attachment.Core.DL;
+
+    /// <summary>
+    /// 附件业务访问服务类
+    /// </summary>
+    internal class AttachmentService : AbstractApiService<Attachment>, IBaseService<Attachment>
+    {
+        #region Constructor
+        /// <summary>
+        /// 附件业务访问服务类
+        /// </summary>
+        /// <param name="controller">控制器</param>
+        public AttachmentService(string controller) : base(controller)
+        {
+        }
+        #endregion //Constructor
+    }
+}
