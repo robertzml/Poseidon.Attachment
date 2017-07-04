0db50ca427f7110a20a47c49ce0f43b9510b41b0
robertzml <robertzml@126.com>
2017-07-04 16:02:49 +0800

    Add Poseidon.Attachment project.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ Poseidon.Attachment.Core/IDAL/IAttachmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poseidon.Attachment.Core.IDAL
+{
+    using Poseidon.Base.Framework;
+    using Poseidon.Attachment.Core.DL;
+
+    /// <summary>
+    /// 附件对象访问接口
+    /// </summary>
+    internal interface IAttachmentRepository : IBaseDAL<Attachment>
+    {
+    }
+}
